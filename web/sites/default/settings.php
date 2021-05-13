2f49e50939ad5e18b4fac771643567dcd99b6edd
Jack Garratt <jack.garratt@icfnext.com>
2021-05-12 20:05:11 -0400

    Exported to config to proper config sync folder

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -785,4 +785,4 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 );
-$settings['config_sync_directory'] = 'config/sync';
+$settings['config_sync_directory'] = '../config/sync';
